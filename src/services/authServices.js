1852f064fcc0702af53c54630c5488e4d41281e9
Ujackline <jackline.umwali@eagles.oc.edu>
2024-11-06 10:40:04 -0600

    edits

--- src/services/authServices.js
+++ src/services/authServices.js
@@ -3,11 +3,11 @@ import apiClient from "./services.js";
 export default {
   loginUser(user) {
     return apiClient.post("login", user)
-      .then(response => response)
-      .catch(error => {
-        console.error("Login error:", error);
-        throw error; // rethrow to allow the calling component to handle
-      });
+      // .then(response => response)
+      // .catch(error => {
+      //   console.error("Login error:", error);
+      //   throw error; // rethrow to allow the calling component to handle
+      // });
   },
   authorizeUser(code) {
     return apiClient.post("authorize", code);
