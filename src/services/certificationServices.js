efad47d29bc7e0759c83b698a1032769f3121e7d
LauraNsenga <laura.nsenga@eagles.oc.edu>
2024-11-14 13:45:12 -0600

    certificate

--- src/services/certificationServices.js
+++ src/services/certificationServices.js
@@ -3,7 +3,7 @@ import apiClient from '../services/services';
 export default {
   // Create a new AwardCertifications
   createCertification(data) {
-    return apiClient.post('/AwardCertifications/', data);
+    return apiClient.post(`/awardCertification/${id}`, data);
   },
 
   // Get all AwardCertificationss for a specific resume
